9ef6b9c360996f47dc4d2fe84e1a30be5aef26be
Ushnika09 <karushnika@gmail.com>
2025-09-09 13:37:17 +0530

    UpdATED METHOD PROM PATCH TO PUT

--- Backend/Routes/VideoRoutes.js
+++ Backend/Routes/VideoRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { getVideos, seedVideos, getVideoById, updateLikes } from "../Controller/VideoController.js";
+import { authenticateToken } from "../Middlewear/AuthToken.js";
+
 
 const router = express.Router();
 
+router.put("/likes/:videoId", authenticateToken, updateLikes);
 router.get("/", getVideos);        // GET all videos
 router.get("/:id", getVideoById);  // GET single video by videoId
 router.post("/seed", seedVideos);  // Seed new videos
-router.patch("/likes/:videoId", updateLikes);// Update like/dislike
 
 export default router;
