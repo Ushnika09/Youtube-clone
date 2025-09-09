96e07961d1615495315eb1f9d1f91f6107ff1194
Ushnika09 <karushnika@gmail.com>
2025-09-09 11:55:51 +0530

    fixed props

--- Frontend/src/layout/Playvideo.jsx
+++ Frontend/src/layout/Playvideo.jsx
@@ -53,7 +53,7 @@ function Playvideo() {
   return (
     <div className={`pl-10 max-w-screen pt-[3.8rem] flex gap-5 ${mode ? "bg-black" : "bg-white"}`}>
       <div className="flex flex-col w-[700px]">
-        <VideoSec video={video} />
+        <VideoSec video={video} id={id}/>
         <Comments id={id} />
       </div>
       <Sugesstion id={id} />
