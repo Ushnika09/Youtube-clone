d24ab1dd53c1f6284628aafc437a42e6e1fb64dd
Ushnika09 <karushnika@gmail.com>
2025-09-10 19:36:40 +0530

    fixed videocard

--- Frontend/src/Components/VideoSec.jsx
+++ Frontend/src/Components/VideoSec.jsx
@@ -1,30 +1,37 @@
-import millify from "millify";
-import React, { useContext } from "react";
-
+import React from "react";
 import YouTube from "react-youtube";
-import ModeContext from "../context/ModeContext";
-import { useData } from "../context/DataContext";
 import Details from "./Details";
 
 function VideoSec({ id, video }) {
-
+  const url = video.videoUrl; // MP4 or video URL
+  const isVideoUrl = url && url.startsWith("http");
 
   return (
     <div className="flex flex-col w-[700px]">
       <div className="w-[700px] h-[380px] relative overflow-hidden rounded-lg">
-        <YouTube
-          className="absolute top-0 left-0 w-full h-full"
-          videoId={id}
-          opts={{
-            height: "100%",
-            width: "100%",
-            playerVars: {
-              autoplay: 1, // no autoplay
-              controls: 1, // show controls
-            },
-          }}
-          onReady={(e) => e.target.pauseVideo()}
-        />
+        {isVideoUrl ? (
+          <video
+            src={url}
+            controls
+            autoPlay
+            loop
+            muted
+            className="absolute top-0 left-0 w-full h-full object-cover"
+          />
+        ) : (
+          <YouTube
+            videoId={id}
+            opts={{
+              height: "100%",
+              width: "100%",
+              playerVars: {
+                autoplay: 1,
+                controls: 1,
+              },
+            }}
+            className="absolute top-0 left-0 w-full h-full"
+          />
+        )}
       </div>
       <Details id={id} video={video} />
     </div>
