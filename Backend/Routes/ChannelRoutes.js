0bf9875766e59bfc0e3fd697de27477b7a69ab26
Ushnika09 <karushnika@gmail.com>
2025-09-10 15:09:26 +0530

    updated routes

--- Backend/Routes/ChannelRoutes.js
+++ Backend/Routes/ChannelRoutes.js
@@ -2,21 +2,27 @@ import express from "express";
 import {
   createChannel,
   addVideoToChannel,
-  getAllChannels,
-  getChannelById,
-  getChannelByUserId
+  getChannelByUserId,
+  deleteVideoFromChannel,
+  updateChannel,
+  updateVideoInChannel
 } from "../Controller/ChennelController.js";
 import { verifyToken } from "../Middlewear/AuthMiddlewear.js";
 
 const router = express.Router();
 
-// ---------------- Public routes ----------------
-router.get("/", getAllChannels);               // Get all channels
-router.get("/:id", getChannelById);           // Get single channel by ID
+//Public routes 
 router.get("/user/:userId", getChannelByUserId); // Get channel by user ID
 
-// ---------------- Protected routes ----------------
+//Protected routes 
 router.post("/", verifyToken, createChannel);                   // Create a new channel
 router.post("/:channelId/video", verifyToken, addVideoToChannel); // Add video to a channel
+router.delete("/:channelId/video/:videoId", verifyToken, deleteVideoFromChannel);
+// Update channel
+router.put("/:channelId", verifyToken, updateChannel);
+
+// Update video
+router.put("/:channelId/video/:videoId", verifyToken, updateVideoInChannel);
+
 
 export default router;
