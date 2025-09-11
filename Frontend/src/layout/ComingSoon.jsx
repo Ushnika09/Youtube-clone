afd2544e847e9daee896087cde09405691ca6c26
Ushnika09 <karushnika@gmail.com>
2025-09-11 13:02:47 +0530

    Update ComingSoon.jsx

--- Frontend/src/layout/ComingSoon.jsx
+++ Frontend/src/layout/ComingSoon.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaYoutube } from "react-icons/fa";
+import ModeContext from "../context/ModeContext"
 
 function ComingSoon({ feature }) {
+  const {mode}=useContext(ModeContext)
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-full text-center px-4">
+    <div className={`flex flex-col items-center justify-center h-screen w-full text-center px-4 ${mode ? "bg-black text-white" :"bg-white text-black"}`}>
       <FaYoutube className="text-red-600 text-9xl mb-6 animate-bounce" />
       <h1 className="text-4xl font-bold mb-4">Coming Soon!</h1>
       <p className="text-lg text-gray-500">
